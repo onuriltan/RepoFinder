58c9de82d15cdd5feeac652d6039d913eda0dae2
onuriltan <onur.iltan@gmail.com>
2019-07-01 10:36:58 +0300

    fetch users from github and pass it down to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import React, {Component} from 'react';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
 
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    let res = await axios.get('https://api.github.com/users');
+    this.setState({users: res.data, loading: true});
+  }
+
+
   render() {
+    const {users, loading} = this.state;
     return (
         <div className="App">
           <Navbar title="Repo Finder" icon="fab fa-github"/>
           <div className="container">
-            <Users/>
+            <Users loading={loading} users={users}/>
           </div>
         </div>
     );
