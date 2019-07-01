1a1ebd45919d0c767c040c68c8c32c5f224f08cc
onuriltan <onur.iltan@gmail.com>
2019-07-01 10:21:34 +0300

    convert some class components to functionals

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,27 +1,24 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types'
+import React from 'react';
+import * as PropTypes from 'prop-types'
 
-class Navbar extends Component {
+const Navbar = ({icon, title}) => {
+  return (
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={icon}/> {title}
+        </h1>
+      </nav>
+  );
+};
 
-  static defaultProps = {
-    title: 'Repo Finder',
-    icon: 'fab fa-github'
-  };
+Navbar.defaultProps = {
+  title: 'Repo Finder',
+  icon: 'fab fa-github'
+};
 
-  static propTypes = { // check props types to make things correct data type
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
-
-  render() {
-    return (
-        <nav className="navbar bg-primary">
-          <h1>
-            <i className={this.props.icon}/> {this.props.title}
-          </h1>
-        </nav>
-    );
-  }
-}
+Navbar.propTypes = { // check props types to make things correct data type
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
