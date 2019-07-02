ba4af2f28e080740652e7b475072a638030797a9
Onur İltan <onur.iltan@C02XH0S8JG5J.local>
2019-07-02 14:37:50 +0300

    add ling to navbar title

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import * as PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Navbar = ({icon, title}) => {
   return (
       <nav className="navbar bg-primary">
-        <h1>
-          <i className={icon}/> {title}
-        </h1>
+        <Link to='/'>
+          <h1>
+            <i className={icon}/> {title}
+          </h1>
+        </Link>
         <ul>
           <li>
             <Link to='/'>Home</Link>
