1e35a3406dda7ff5f379cbf37c5c1901d4f397a3
onuriltan <onur.iltan@gmail.com>
2019-07-07 13:46:09 +0300

    context api implemented

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,13 +1,12 @@
 import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner'
 import Repos from '../repos/Repos'
-import PropTypes from 'prop-types'
 import { Link } from "react-router-dom";
 import GithubContext from '../../context/github/githubContext'
 
-const User = ({ getUserRepos, repos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext)
-  const { getUser, loading, user } = githubContext
+  const { getUserRepos, repos, getUser, loading, user } = githubContext
 
   useEffect(() => {
     getUser(match.params.login);
@@ -82,8 +81,5 @@ const User = ({ getUserRepos, repos, match }) => {
   }
 };
 
-User.propTypes = {
-  getUserRepos: PropTypes.func.isRequired
-};
 
 export default User;
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -35,6 +35,13 @@ export default (state, action) => {
         loading: false
       }
 
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false
+      }
+
     default:
       return state
 
